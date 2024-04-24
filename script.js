962db149ded6065a18a5616e6572859379d7b7ef
vinaharvani <vinaharvani2000@gmail.com>
2024-04-24 23:49:25 +0530

    advice-generator-app

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = populateQuote()
+
+let quoteNumber = document.getElementById('quote-number')
+let quote =document.getElementById('quote')
+let quoteBtn =document.getElementById('generate-quote-btn')
+
+quoteBtn.addEventListener('click',populateQuote)
+
+function populateQuote() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(Response=>{
+        return Response.json();
+    })
+    .then(data=>{
+        let name = data.slip;
+        quoteNumber.innerHTML = name.id;
+        quote.innerHTML = name.advice;
+    })
+}
+
